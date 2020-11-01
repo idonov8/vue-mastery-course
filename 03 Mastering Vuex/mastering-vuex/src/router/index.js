2732099c2edc9ee36031bef28b5d5e29431721ca
Ido Nov <idonov8@gmail.com>
2020-11-01 16:52:49 +0200

    initializing 03 mastering vue

---
+++ 03 Mastering Vuex/mastering-vuex/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventCreate from '@/views/EventCreate.vue'
+import EventList from '@/views/EventList.vue'
+import EventShow from '@/views/EventShow.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'event-list',
+    component: EventList
+  },
+  {
+    path: '/event/:id',
+    name: 'event-show',
+    component: EventShow,
+    props: true
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router

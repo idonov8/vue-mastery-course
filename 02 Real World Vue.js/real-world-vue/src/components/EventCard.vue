2409506f901f2c0bc2b3930e8783d24116ebd998
Ido Nov <idonov8@gmail.com>
2020-11-01 12:59:54 +0200

    Styling EventCard component and adding data

--- 02 Real World Vue.js/real-world-vue/src/components/EventCard.vue
+++ 02 Real World Vue.js/real-world-vue/src/components/EventCard.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h4>{{ title }}</h4>
+  <div class="event-card -shadow">
+    <span class="eyebrow">@{{ event.time }} on {{ event.date }} </span>
+    <h4 class="title">{{ event.title }}</h4>
+    <span>{{ event.attendees.length }} attending </span>
   </div>
 </template>
 
@@ -8,14 +10,38 @@
 export default {
   data() {
     return {
-      title: 'Park Cleanup'
+      event: {
+        id: 1,
+        title: 'Beach Cleanup',
+        date: 'Tues Nov 19, 2020',
+        time: '6:00',
+        attendees: [
+          { id: 'abc123', name: 'Ido Nov' },
+          { id: 'def456', name: 'Tamar Peretz' }
+        ]
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-h4 {
-  color: green;
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
 }
 </style>

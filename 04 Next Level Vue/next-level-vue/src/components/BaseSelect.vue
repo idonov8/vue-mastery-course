220123b26b160d258ec25af0e5890879dbbd071f
Ido Nov <idonov8@gmail.com>
2020-11-08 11:07:04 +0200

    08 Base Select

---
+++ 04 Next Level Vue/next-level-vue/src/components/BaseSelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <label for="" v-if="label">{{ label }} </label>
+    <select type="text" :value="value" @input="updateValue" v-bind="$attrs">
+      <option
+        v-for="option in options"
+        :key="option"
+        :selected="option === value"
+        >{{ option }}</option
+      >
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    options: {
+      type: Array
+    },
+    value: [String, Number]
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- 04 Next Level Vue/next-level-vue/src/views/EventCreate.vue
+++ 04 Next Level Vue/next-level-vue/src/views/EventCreate.vue
@@ -2,10 +2,12 @@
   <div>
     <h1>Create an Event</h1>
     <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-      </select>
+      <BaseSelect
+        v-model="event.category"
+        label="Select a category"
+        :options="categories"
+        :selected="option === value"
+      />
 
       <h3>Name & describe your event</h3>
       <div class="field">
@@ -40,12 +42,13 @@
         <label>Date</label>
         <datepicker v-model="event.date" placeholder="Select a date" />
       </div>
-      <div class="field">
-        <label>Select a time</label>
-        <select v-model="event.time">
-          <option v-for="time in times" :key="time">{{ time }}</option>
-        </select>
-      </div>
+
+      <BaseSelect
+        v-model="event.time"
+        label="Select a time"
+        :options="times"
+        class="field"
+      />
       <input type="submit" class="button -fill-gradient" value="Submit" />
     </form>
   </div>

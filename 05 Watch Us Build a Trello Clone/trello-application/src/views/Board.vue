c2e08ea071cb8237b4bfd09d29641ebce0637e28
Ido Nov <idonov8@gmail.com>
2020-11-10 17:11:43 +0200

    05 implement drag & drop for tasks

--- 05 Watch Us Build a Trello Clone/trello-application/src/views/Board.vue
+++ 05 Watch Us Build a Trello Clone/trello-application/src/views/Board.vue
@@ -3,16 +3,22 @@
     <div class="flex flex-row items-start">
      <div
      class="column"
-     v-for="(column, index) in board.columns"
-     :key="index">
+     v-for="(column, columnIndex) in board.columns"
+     :key="columnIndex"
+     @drop="moveTask($event, column.tasks)"
+     @dragover.prevent
+     @dragenter.prevent
+     >
        <div class="flex items-center mb-2 font-bold">
          {{column.name}}
        </div>
        <div class="list-reset">
          <div
          class="task"
-         v-for="(task, index) in column.tasks"
-         :key="index"
+         v-for="(task, taskIndex) in column.tasks"
+         :key="taskIndex"
+         draggable
+         @dragstart="pickupTask($event, taskIndex, columnIndex)"
          @click="goToTask(task)"
          >
           <span class="w-full flex-no-shrink fond-bold">
@@ -63,6 +69,26 @@ export default {
         name: event.target.value
       })
       event.target.value = ''
+    },
+    pickupTask (event, taskIndex, fromColumnIndex) {
+      event.dataTransfer.effectAllowed = 'move'
+      event.dataTransfer.dropEffect = 'move'
+
+      event.dataTransfer.setData('task-index', taskIndex)
+      event.dataTransfer.setData('from-column-index', fromColumnIndex)
+    },
+    moveTask (event, toTasks) {
+      const fromColumnIndex = event.dataTransfer.getData('from-column-index')
+      const fromTasks = this.board.columns[fromColumnIndex].tasks
+      const taskIndex = event.dataTransfer.getData('task-index')
+
+      this.$store.commit('MOVE_TASK', {
+        fromTasks,
+        toTasks,
+        taskIndex
+
+      })
+      fromColumn.push()
     }
   }
 

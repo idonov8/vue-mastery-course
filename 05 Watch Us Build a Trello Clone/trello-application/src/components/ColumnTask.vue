35d7c405bf6909865217d4a727b1be6ef238505b
Ido Nov <idonov8@gmail.com>
2020-11-13 07:42:18 +0200

    09 Refactor to use a mixin and reduce code repetition

--- 05 Watch Us Build a Trello Clone/trello-application/src/components/ColumnTask.vue
+++ 05 Watch Us Build a Trello Clone/trello-application/src/components/ColumnTask.vue
@@ -19,25 +19,16 @@
 </template>
 
 <script>
+import movingTasksAndColumnsMixin from '@/mixins/movingTasksAndColumnsMixin'
+
 export default {
+  mixins: [movingTasksAndColumnsMixin],
   props: {
-    board: {
-      type: Object,
-      required: true
-    },
     task: {
       type: Object,
       required: true
     },
     taskIndex: {
-      type: Number,
-      required: true
-    },
-    column: {
-      type: Object,
-      required: true
-    },
-    columnIndex: {
       type: Number,
       required: true
     }
@@ -53,33 +44,6 @@ export default {
     },
     goToTask (task) {
       this.$router.push({ name: 'task', params: { id: task.id } })
-    },
-    moveTaskOrColumn (event, toTasks, toColumnIndex, toTaskIndex) {
-      const type = event.dataTransfer.getData('type')
-      if (type === 'task') {
-        this.moveTask(event, toTasks, toTaskIndex !== undefined ? toTaskIndex : toTasks.length)
-      } else {
-        this.moveColumn(event, toColumnIndex)
-      }
-    },
-    moveTask (event, toTasks, toTaskIndex) {
-      const fromColumnIndex = event.dataTransfer.getData('from-column-index')
-      const fromTasks = this.board.columns[fromColumnIndex].tasks
-      const fromTaskIndex = event.dataTransfer.getData('from-task-index')
-
-      this.$store.commit('MOVE_TASK', {
-        fromTasks,
-        fromTaskIndex,
-        toTasks,
-        toTaskIndex
-      })
-    },
-    moveColumn (event, toColumnIndex) {
-      const fromColumnIndex = event.dataTransfer.getData('from-column-index')
-      this.$store.commit('MOVE_COLUMN', {
-        fromColumnIndex,
-        toColumnIndex
-      })
     }
   }
 }

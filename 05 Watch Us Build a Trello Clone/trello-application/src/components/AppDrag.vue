6e09598f4acde43d5a1e9f2d507a629a36ba5307
Ido Nov <idonov8@gmail.com>
2020-11-15 22:03:21 +0200

    10 Add AppDrag component for encapsulating drag api

---
+++ 05 Watch Us Build a Trello Clone/trello-application/src/components/AppDrag.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+        @dragstart.self="onDrag"
+        draggable
+        @dragover.prevent
+        @dragenter.prevent
+    >
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    transferData: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    onDrag (event) {
+      event.dataTransfer.setData('payload', JSON.stringify(this.transferData))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

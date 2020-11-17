21a05139508502297b5cc6a6e5f94bc6a81a7c4d
Ido Nov <idonov8@gmail.com>
2020-11-17 15:45:38 +0200

    05 JsavaScript Hooks and Velocity library

--- 06 Animating Vue/animating-vue/src/views/Drawer.vue
+++ 06 Animating Vue/animating-vue/src/views/Drawer.vue
@@ -21,10 +21,31 @@
 </template>
 
 <script>
+import Velocity from 'velocity-animate'
 export default {
   data() {
     return {
       isOpen: false
+    }
+  },
+  methods: {
+    beforeEnter(el) {
+      el.style.opacity = 0
+      el.style.width = '0em'
+    },
+    enter(el, done) {
+      Velocity(
+        el,
+        { opacity: 1, width: '12em' },
+        { duration: 1000, easing: [60, 10], complete: done }
+      )
+    },
+    leave(el, done) {
+      Velocity(
+        el,
+        { opacity: 0, width: '0em' },
+        { duration: 500, easing: 'easeInCubic', complete: done }
+      )
     }
   }
 }

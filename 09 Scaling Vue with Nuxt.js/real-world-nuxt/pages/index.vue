d808b77dac29776fe70830b5a341f3d47d674e9f
Ido Nov <idonov8@gmail.com>
2020-11-24 19:43:47 +0200

    06 API calls with axios

--- 09 Scaling Vue with Nuxt.js/real-world-nuxt/pages/index.vue
+++ 09 Scaling Vue with Nuxt.js/real-world-nuxt/pages/index.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
     <h1>Events</h1>
+
+    <event-card
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '~/components/EventCard.vue'
+
 export default {
+  components: { EventCard },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events, please try again later',
+        })
+      })
+  },
   head() {
     return {
       title: 'Event Listing',

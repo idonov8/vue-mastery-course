8322b63fa91e55d4b9f81d8a0aaa92e48ab72cc2
Ido Nov <idonov8@gmail.com>
2020-11-24 20:24:49 +0200

    08 Vuex

--- 09 Scaling Vue with Nuxt.js/real-world-nuxt/pages/index.vue
+++ 09 Scaling Vue with Nuxt.js/real-world-nuxt/pages/index.vue
@@ -12,16 +12,14 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import EventCard from '~/components/EventCard.vue'
 
 export default {
   components: { EventCard },
-  async asyncData({ $axios, error }) {
+  async fetch({ store, error }) {
     try {
-      const { data } = await $axios.get('http://localhost:3000/events')
-      return {
-        events: data,
-      }
+      await store.dispatch('events/fetchEvents')
     } catch (e) {
       error({
         statusCode: 503,
@@ -29,6 +27,9 @@ export default {
       })
     }
   },
+  computed: mapState({
+    events: (state) => state.events.events,
+  }),
   head() {
     return {
       title: 'Event Listing',

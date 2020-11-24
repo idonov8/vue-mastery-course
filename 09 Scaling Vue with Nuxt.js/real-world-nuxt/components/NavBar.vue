e0ef6213e29601eb377fe9a8cef0afad24a3c891
Ido Nov <idonov8@gmail.com>
2020-11-24 19:03:34 +0200

    05 Dynamic routing and error page

--- 09 Scaling Vue with Nuxt.js/real-world-nuxt/components/NavBar.vue
+++ 09 Scaling Vue with Nuxt.js/real-world-nuxt/components/NavBar.vue
@@ -2,7 +2,7 @@
   <div class="nav">
     <nuxt-link to="/" class="brand"> Real World Events :D </nuxt-link> |
     <nuxt-link to="/"> List </nuxt-link> |
-    <nuxt-link to="/create"> Create </nuxt-link>
+    <nuxt-link to="/event/create"> Create </nuxt-link>
   </div>
 </template>
 

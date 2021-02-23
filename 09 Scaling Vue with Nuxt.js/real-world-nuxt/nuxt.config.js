bf0d0481f90ecec4304f687544b8d03c81aa4ea9
Ido Nov <idonov8@gmail.com>
2021-02-23 16:52:34 +0200

    starting code

--- 09 Scaling Vue with Nuxt.js/real-world-nuxt/nuxt.config.js
+++ 09 Scaling Vue with Nuxt.js/real-world-nuxt/nuxt.config.js
@@ -1,3 +1,4 @@
+import EventService from 'services/EventService.js'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -39,4 +40,14 @@ export default {
 
   // Progress bar color
   loading: { color: '#39b982' },
+
+  generate: {
+    routes: () => {
+      return EventService.getEvents().then((response) => {
+        return response.data.map((event) => {
+          return '/event/' + event.id
+        })
+      })
+    },
+  },
 }
